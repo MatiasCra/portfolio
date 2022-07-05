f3176927e07d849182b353c938f789c90299164c
MatiasCra <cravchikm@gmail.com>
2022-07-05 19:59:39 -0300

    sendgrid mail

--- components/contact.js
+++ components/contact.js
@@ -1,67 +1,97 @@
-import React from "react";
+import React from 'react'
 
 const Contact = () => {
+  const sendMail = async (e) => {
+    e.preventDefault()
+    const data = [
+      { name: document.getElementById('nameInput').value },
+      { email: document.getElementById('emailInput').value },
+      { subject: document.getElementById('subjectInput').value },
+      { message: document.getElementById('messageInput').value },
+    ]
+
+    const formData = {}
+    data.forEach((field) => {
+      const name = Object.keys(field)[0]
+      const value = Object.values(field)[0]
+      if (!value) {
+        return
+      }
+      formData[name] = value
+    })
+
+    if (Object.keys(formData).length < data.length) return
+
+    const res = await fetch('http://localhost:3000/api/send-mail', {
+        method: 'POST',
+        body: JSON.stringify(formData)
+      }
+    )
+  }
+
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center py-8 w-11/12 mx-auto">
-      <div className="w-11/12 lg:w-4/5 xl:w-3/5 mx-auto" id="contact">
-        <h1 className="uppercase text-6xl w-full text-center font-mono">
+    <div className='min-h-screen flex flex-col items-center justify-center py-8 w-11/12 mx-auto'>
+      <div className='w-11/12 lg:w-4/5 xl:w-3/5 mx-auto' id='contact'>
+        <h1 className='uppercase text-6xl w-full text-center font-mono'>
           Contact Me
         </h1>
-        <form className="lg:mt-8 mt-3">
-          <div className="flex flex-col lg:flex-row justify-between">
-            <div className="w-full lg:w-1/2">
-              <label className="flex flex-col lg:flex-row lg:items-center w-full">
-                <div className="lg:inline">
-                  Name<span className="ml-1 text-[#E0AF68]">*</span>
+        <form className='lg:mt-8 mt-3'>
+          <div className='flex flex-col lg:flex-row justify-between'>
+            <div className='w-full lg:w-1/2'>
+              <label className='flex flex-col lg:flex-row lg:items-center w-full'>
+                <div className='lg:inline'>
+                  Name<span className='ml-1 text-[#E0AF68]'>*</span>
                 </div>
                 <input
-                  type="text"
-                  className="w-full lg:ml-2 lg:w-5/6 p-2 rounded-md bg-zinc-900 border-2 border-zinc-400 transition-shadow ease-in-out focus-visible:outline-0 duration-300 focus:outline-none focus:shadow-[0_0_0_1px_ghostwhite]"
+                  id='nameInput'
+                  type='text'
+                  className='w-full lg:ml-2 lg:w-5/6 p-2 rounded-md bg-zinc-900 border-2 border-zinc-400 transition-shadow ease-in-out focus-visible:outline-0 duration-300 focus:outline-none focus:shadow-[0_0_0_1px_ghostwhite]'
                 />
               </label>
             </div>
-            <div className="w-full lg:w-1/2 lg:text-right">
-              <label className="flex flex-col lg:flex-row items-start lg:items-center w-full">
-                <div className="lg:inline lg:ml-auto">
-                  Email<span className="ml-1 text-[#E0AF68]">*</span>
+            <div className='w-full lg:w-1/2 lg:text-right'>
+              <label className='flex flex-col lg:flex-row items-start lg:items-center w-full'>
+                <div className='lg:inline lg:ml-auto'>
+                  Email<span className='ml-1 text-[#E0AF68]'>*</span>
                 </div>
                 <input
-                  type="email"
-                  className="w-full lg:ml-2 lg:w-5/6 p-2 rounded-md bg-zinc-900 border-2 border-zinc-400 transition-shadow ease-in-out focus-visible:outline-0 duration-300 focus:outline-none focus:shadow-[0_0_0_1px_ghostwhite]"
+                  id='emailInput'
+                  type='email'
+                  className='w-full lg:ml-2 lg:w-5/6 p-2 rounded-md bg-zinc-900 border-2 border-zinc-400 transition-shadow ease-in-out focus-visible:outline-0 duration-300 focus:outline-none focus:shadow-[0_0_0_1px_ghostwhite]'
                 />
               </label>
             </div>
           </div>
 
-          <label className="block mt-2">
-            Subject<span className="ml-1 text-[#E0AF68]">*</span>
+          <label className='block mt-2'>
+            Subject<span className='ml-1 text-[#E0AF68]'>*</span>
             <input
-              type="text"
-              className="w-full mt-1 p-2 rounded-md bg-zinc-900 border-2 border-zinc-400 transition-shadow ease-in-out focus-visible:outline-0 duration-300 focus:outline-none focus:shadow-[0_0_0_1px_ghostwhite]"
+              id='subjectInput'
+              type='text'
+              className='w-full mt-1 p-2 rounded-md bg-zinc-900 border-2 border-zinc-400 transition-shadow ease-in-out focus-visible:outline-0 duration-300 focus:outline-none focus:shadow-[0_0_0_1px_ghostwhite]'
             />
           </label>
 
-          <label className="block mt-2">
-            Message<span className="ml-1 text-[#E0AF68]">*</span>
+          <label className='block mt-2'>
+            Message<span className='ml-1 text-[#E0AF68]'>*</span>
             <textarea
-              rows="10"
-              columns="50"
-              className="w-full mt-1 p-2 rounded-md bg-zinc-900 border-2 border-zinc-400 transition-shadow ease-in-out focus-visible:outline-0 duration-300 focus:outline-none focus:shadow-[0_0_0_1px_ghostwhite]"
+              id='messageInput'
+              rows='10'
+              columns='50'
+              className='w-full mt-1 p-2 rounded-md bg-zinc-900 border-2 border-zinc-400 transition-shadow ease-in-out focus-visible:outline-0 duration-300 focus:outline-none focus:shadow-[0_0_0_1px_ghostwhite]'
             ></textarea>
           </label>
 
           <button
-            onClick={(e) => {
-              e.preventDefault();
-            }}
-            className="p-3 mt-2 rounded-full bg-[#7AA2F7] text-zinc-900 font-bold box-border transition-all ease-in-out duration-200 hover:scale-[1.15] hover:bg-transparent hover:text-[#7AA2F7] border-4 border-transparent hover:border-[#7AA2F7]"
+            onClick={sendMail}
+            className='p-3 mt-2 rounded-full bg-[#7AA2F7] text-zinc-900 font-bold box-border transition-all ease-in-out duration-200 hover:scale-[1.15] hover:bg-transparent hover:text-[#7AA2F7] border-4 border-transparent hover:border-[#7AA2F7]'
           >
             Send Message
           </button>
         </form>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Contact;
+export default Contact
---
+++ pages/api/send-mail.js
@@ -0,0 +1,23 @@
+export default async function handle(req, res) {
+  const sgMail = require('@sendgrid/mail')
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+  const body = JSON.parse(req.body)
+  const msg = {
+    to: 'cravchikm@gmail.com', // Change to your recipient
+    from: 'cravchikm@gmail.com', // Change to your verified sender
+    subject: body.subject,
+    text: `Message from ${body.name} on portfolio\n${body.message}`,
+    html: `Message from ${body.name} on portfolio<br/>${body.message}`,
+  }
+  sgMail
+    .send(msg)
+    .then((response) => {
+      console.log(response[0].statusCode)
+      console.log(response[0].headers)
+      res.status(200).json({ status: 'Ok' })
+    })
+    .catch((error) => {
+      console.error(JSON.stringify(error, 2))
+    })
+}
